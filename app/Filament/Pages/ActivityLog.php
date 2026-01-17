285f3536b20fa398e0330f9d31b8a47cc05bcc18
Oluwatosin Jeremiah <jerexbambex@gmail.com>
2026-01-16 23:10:36 -0700

    fix: correct navigationIcon type declaration in ActivityLog page

--- app/Filament/Pages/ActivityLog.php
+++ app/Filament/Pages/ActivityLog.php
@@ -2,12 +2,13 @@
 
 namespace App\Filament\Pages;
 
+use BackedEnum;
 use pxlrbt\FilamentActivityLog\Pages\ListActivities;
 use UnitEnum;
 
 class ActivityLog extends ListActivities
 {
-    protected static ?string $navigationIcon = 'heroicon-o-clipboard-document-list';
+    protected static BackedEnum|string|null $navigationIcon = 'heroicon-o-clipboard-document-list';
 
     protected static UnitEnum|string|null $navigationGroup = 'Administration';
 
