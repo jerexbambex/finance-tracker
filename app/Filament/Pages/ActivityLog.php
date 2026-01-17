f55896ed4bd67b88125e5fd2ec9b8c57d74c2e17
Oluwatosin Jeremiah <jerexbambex@gmail.com>
2026-01-16 23:09:56 -0700

    fix: correct navigationGroup type declaration in ActivityLog page

--- app/Filament/Pages/ActivityLog.php
+++ app/Filament/Pages/ActivityLog.php
@@ -3,12 +3,13 @@
 namespace App\Filament\Pages;
 
 use pxlrbt\FilamentActivityLog\Pages\ListActivities;
+use UnitEnum;
 
 class ActivityLog extends ListActivities
 {
     protected static ?string $navigationIcon = 'heroicon-o-clipboard-document-list';
 
-    protected static ?string $navigationGroup = 'Administration';
+    protected static UnitEnum|string|null $navigationGroup = 'Administration';
 
     protected static ?int $navigationSort = 4;
 }
