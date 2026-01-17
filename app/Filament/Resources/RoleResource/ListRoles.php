201ff322762cab2a70b3ca219c239ba9e4d8b498
Oluwatosin Jeremiah <jerexbambex@gmail.com>
2026-01-16 19:59:02 -0700

    fix: correct RoleResource import in page classes
    - Fixed duplicate RoleResource namespace in imports
    - Changed from RoleResource\RoleResource to just RoleResource

--- app/Filament/Resources/RoleResource/ListRoles.php
+++ app/Filament/Resources/RoleResource/ListRoles.php
@@ -2,7 +2,7 @@
 
 namespace App\Filament\Resources\RoleResource\Pages;
 
-use App\Filament\Resources\RoleResource\RoleResource;
+use App\Filament\Resources\RoleResource;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
