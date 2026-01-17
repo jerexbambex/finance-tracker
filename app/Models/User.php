50271aa588e565a1dba5785c53124381d06a7654
Oluwatosin Jeremiah <jerexbambex@gmail.com>
2026-01-16 19:48:41 -0700

    feat: install and configure Spatie Laravel Permission
    - Installed spatie/laravel-permission package
    - Published config and migrations
    - Configured to use UUIDs (model_uuid)
    - Added HasRoles trait to User model
    - Ran migrations for roles and permissions tables

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,11 +8,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, HasUuids, Notifiable, TwoFactorAuthenticatable;
+    use HasFactory, HasUuids, Notifiable, TwoFactorAuthenticatable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
