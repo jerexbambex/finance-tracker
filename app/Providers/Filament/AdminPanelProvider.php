d71374693c6770a68a3bb84703349f88037af843
Oluwatosin Jeremiah <jerexbambex@gmail.com>
2026-01-16 23:23:00 -0700

    feat: manually register ActivityResource in admin panel

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -30,6 +30,9 @@ class AdminPanelProvider extends PanelProvider
                 'primary' => Color::Amber,
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
+            ->resources([
+                \Jacobtims\FilamentLogger\Resources\ActivityResource::class,
+            ])
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
             ->pages([
                 Dashboard::class,
