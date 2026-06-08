6487cab17a53b0d533614d817c3fe11b8b949b63
Oluwatosin Jeremiah <jerexbambex@gmail.com>
2026-06-08 09:14:51 -0500

    fix(filament): move user transactions chart under the stats
    Render the chart as a header widget (right below the stats) instead of a
    footer widget at the bottom of the page.

--- app/Filament/Resources/Users/Pages/ViewUser.php
+++ app/Filament/Resources/Users/Pages/ViewUser.php
@@ -23,12 +23,6 @@ class ViewUser extends ViewRecord
     {
         return [
             UserStatsWidget::class,
-        ];
-    }
-
-    protected function getFooterWidgets(): array
-    {
-        return [
             UserTransactionsChart::class,
         ];
     }
